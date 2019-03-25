720f7a014e9d743e760eea08086ae949d5a5b90d
Bogdan <bogdan7morozyuk@gmail.com>
2019-03-25 17:50:00 +0200

    Add overlay to modal

--- Modal/index.js
+++ Modal/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Modal = ({ title, text }) => (
     <section className="modal">
-        <header className="modal__header">
-            <h3 className="modal__title">
-                {title}
-            </h3>
-        </header>
-        <main className="modal__body">
-            <p className="modal__text">
-                {text}
-            </p>
-            <div className="modal__loader" />
-        </main>
+        <div className="modal__inner">
+            <header className="modal__header">
+                <h3 className="modal__title">{title}</h3>
+            </header>
+            <main className="modal__body">
+                <p className="modal__text">{text}</p>
+                <div className="modal__loader" />
+            </main>
+        </div>
     </section>
 );
 
 Modal.defaultProps = {
-    title: 'Message',
+    title: "Message"
 };
 
 export default Modal;
