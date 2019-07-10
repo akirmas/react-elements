9b1f9d1b2558e914923eea688082dbe7322802b2
akirmas <andrii@gobe-mark.com>
2019-07-10 18:28:29 +0300

    Move out flat things

--- Payment/Section/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default ({active=false, children}) =>
-  <div className={'Payment Section ' + (active !== 'false' ? 'Active' : 'Passive')}>
-    {children}
-  </div>
